dde4133cb2597adbc408419b62e014d31e3c8324
P0wd3r696 <saziyenge@gmail.com>
2019-04-04 13:08:52 +0200

    Add project files.

---
+++ TimerApp/TimerApp/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TimerApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Device.StartTimer(TimeSpan.FromSeconds(1), OnTimerTick);
+        }
+
+        bool OnTimerTick()
+        {
+            DateTime dt = DateTime.Now;
+            timerLabel.Text = dt.ToString("T");
+            dateLabel.Text = dt.ToString("D");
+
+            return true;
+        }
+    }
+}
